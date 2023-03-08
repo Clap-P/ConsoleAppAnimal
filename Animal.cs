11f71a59c0ec3b59368845037b48b917f20c03d8
Pierre Clapisson <arkceus76300@gmail.com>
2023-03-08 09:55:57 +0100

    Ajoutez des fichiers projet.

---
+++ Animal.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal("Lion");
+        myAnimal.Move();
+        myAnimal.Sleep();
+
+        Animal anotherAnimal = new Animal();
+        anotherAnimal.Name = "Tiger";
+        anotherAnimal.Move();
+        anotherAnimal.Sleep();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Animal
+{
+    public string Name { get; set; }
+
+    public Animal()
+    {
+        Name = "Lukas";
+    }
+
+    public Animal(string name)
+    {
+        Name = name;
+    }
+
+    public void Move()
+    {
+        Console.WriteLine($"{Name} is moving.");
+    }
+
+    public void Sleep()
+    {
+        Console.WriteLine($"{Name} is sleeping.");
+    }
+}
