a4dd50274d3690e2250a273cee7b06324c1f8811
Pierre Clapisson <arkceus76300@gmail.com>
2023-03-08 11:26:43 +0100

    Commit 1

--- Animal.cs
+++ Animal.cs
@@ -2,13 +2,21 @@
 {
     static void Main(string[] args)
     {
-        Animal myAnimal = new Animal("Lion");
-        myAnimal.Move();
-        myAnimal.Sleep();
+        //Animal myAnimal = new Animal("Roucoups");
+        //myAnimal.Move();
+        //myAnimal.Sleep();
 
-        Animal anotherAnimal = new Animal();
-        anotherAnimal.Name = "Tiger";
-        anotherAnimal.Move();
-        anotherAnimal.Sleep();
+        //Animal anotherAnimal = new Animal();
+        //anotherAnimal.Name = "Groudon";
+        //anotherAnimal.Move();
+        //anotherAnimal.Sleep();
+
+        Dragon myDragon = new Dragon("Dragon");
+        myDragon.Move();
+        myDragon.Sleep();
+
+        Griffin myGriffin = new Griffin("Griffin");
+        myGriffin.Move();
+        myGriffin.Sleep();
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-public class Animal
+public abstract class Animal
 {
     public string Name { get; set; }
 
     public Animal()
     {
-        Name = "Lukas";
+        Name = "Random";
     }
 
     public Animal(string name)
@@ -14,13 +14,42 @@ public class Animal
         Name = name;
     }
 
-    public void Move()
-    {
-        Console.WriteLine($"{Name} is moving.");
-    }
+    public abstract void Move();
 
     public void Sleep()
     {
-        Console.WriteLine($"{Name} is sleeping.");
+        Console.WriteLine($"{Name} est endormi.");
+    }
+}
+
+public class Dragon : Animal
+{
+    public Dragon() : base()
+    {
+    }
+
+    public Dragon(string name) : base(name)
+    {
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine($"{Name} vole dans les airs.");
+    }
+}
+
+public class Griffin : Animal
+{
+    public Griffin() : base()
+    {
+    }
+
+    public Griffin(string name) : base(name)
+    {
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine($"{Name} s'envole .");
     }
 }
